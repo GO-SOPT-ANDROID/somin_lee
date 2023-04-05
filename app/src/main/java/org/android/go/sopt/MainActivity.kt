43467d8ea35345a818036aeaecff20268ff0483d
salth <salth6@naver.com>
2023-04-05 22:46:40 +0900

    feat : 심화 과제 완료

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -2,17 +2,27 @@ package org.android.go.sopt
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import org.android.go.sopt.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
-import org.android.go.sopt.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding //바인딩 객체 선언 (자동생성됨)
     private lateinit var getResultText:ActivityResultLauncher<Intent>
+    lateinit var answer_nickname : String
+    lateinit var answer_tmi : String
 
+    // 화면 터치 시 키보드 내리기
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val imm: InputMethodManager =
+            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        return super.dispatchTouchEvent(ev)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) // 액티비티에서 사용할 바인딩 클래스의 객체화
@@ -20,11 +30,14 @@ class MainActivity : AppCompatActivity() {
         var answer_id : String? = null
         var answer_pw : String? = null
 
-        //Activity Result에 콜백 등록 (MainActivity.kt)
+
+        //Activity Result에 콜백 등록
         getResultText = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
                 answer_id = result.data?.getStringExtra("data_id")?:"" // 얘는 엘비스 연산자 nullable하다는 뜻 -> 얘만 써
-                answer_pw = result.data?.getStringExtra("data_pw")?:"" // !! -> nonNullable하도록 만들어 줌
+                answer_pw = result.data?.getStringExtra("data_pw")?:""
+                answer_nickname = result.data?.getStringExtra("data_nickname")?:"" // !! -> nonNullable하도록 만들어 줌
+                answer_tmi = result.data?.getStringExtra("data_tmi")?:""
             }
             else{
                 Snackbar.make(binding.root, "회원 정보가 없습니다", Snackbar.LENGTH_SHORT).show()
@@ -36,14 +49,24 @@ class MainActivity : AppCompatActivity() {
             val input_id = binding.editId.text.toString()
             val input_pw = binding.editPw.text.toString()
             // 입력된 정보가 다를 경우
-            if (answer_id == null || answer_pw == null){ //->얘네 when으로 바꾸면 더 조음
+
+            when {
+                (answer_id == null) || (answer_pw == null)->{
                 Snackbar.make(binding.root, "회원가입을 진행해주세요.", Snackbar.LENGTH_SHORT).show()
-            }
-            else if (input_id != answer_id || input_pw != answer_pw) {
-                Snackbar.make(binding.root, "아이디와 비밀번호를 확인해주세요.", Snackbar.LENGTH_SHORT).show()
-            } else {
-                Snackbar.make(binding.root, "로그인 합니다", Snackbar.LENGTH_SHORT).show()
-                val intent = Intent(this, SignUpActivity::class.java) // 자기소개로 이동
+                }
+                (input_id != answer_id) || (input_pw != answer_pw)->{
+                    Snackbar.make(binding.root, "아이디와 비밀번호를 확인해주세요.", Snackbar.LENGTH_SHORT).show()
+                }
+                else->{
+                    Snackbar.make(binding.root, "로그인 합니다", Snackbar.LENGTH_SHORT).show()
+                    val intent = Intent(this, IntroduceActivity::class.java) // 자기소개로 이동
+                    intent.putExtra("data_id", answer_id)
+                    intent.putExtra("data_pw", answer_pw)
+                    intent.putExtra("data_nickname", answer_nickname)
+                    intent.putExtra("data_tmi", answer_tmi)
+                    finish()
+                    startActivity(intent)
+                }
             }
         }
         // 회원가입 버튼
@@ -57,7 +80,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
 //val nextIntent = Intent(this, SignUpActivity::class.java) // SignUpActivity로 이동
 //startActivity(nextIntent)
 
