97f019e26aac35cebc1a352f4298edda44d48774
salth <salth6@naver.com>
2023-04-05 22:46:40 +0900

    feat : 심화 과제 완료

--- app/src/main/java/org/android/go/sopt/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/SignUpActivity.kt
@@ -2,13 +2,23 @@ package org.android.go.sopt
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import org.android.go.sopt.MainActivity
 import org.android.go.sopt.databinding.ActivitySignUpBinding
 import com.google.android.material.snackbar.Snackbar
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding:ActivitySignUpBinding
+
+    // 화면 터치 시 키보드 내리기
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val imm: InputMethodManager =
+            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        return super.dispatchTouchEvent(ev)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -18,29 +28,28 @@ class SignUpActivity : AppCompatActivity() {
             val id = binding.editId.text.toString()
             val pw = binding.editPw.text.toString()
             val nickname = binding.editNickname.text.toString()
+            val tmi = binding.editTmi.text.toString()
 
-            // 유저가 항목을 다 채우지 않았을 경우
-            if(id.isEmpty() || pw.isEmpty() || nickname.isEmpty()){
+            // 조건에 따라 분기
+            when{
+                (id.isEmpty()) || (pw.isEmpty()) || (nickname.isEmpty()) || (tmi.isEmpty())->{
                 Snackbar.make(binding.root,"공란이 있습니다.",Snackbar.LENGTH_SHORT).show()
-            }
-            else if(id.length<6 || id.length>10){
-                Snackbar.make(binding.root,"아이디는 6~10글자 입니다.",Snackbar.LENGTH_SHORT).show()
-            }
-            else if(pw.length<8 || pw.length>12){
-                Snackbar.make(binding.root,"비밀번호는 8~12글자 입니다.",Snackbar.LENGTH_SHORT).show()
-            }
-            else {
-                var intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("data_id", id)
-                intent.putExtra("data_pw", pw)
-                intent.putExtra("data_nickname", nickname)
-
-                setResult(RESULT_OK,intent)
-                finish() //startActivity(intent) // 얘랑 finish()얘랑 차이점이 머임?
-
-//              intent.putExtra("data_id", binding.editId.text.toString())
-//              intent.putExtra("data_pw", binding.editPw.text.toString())
-//              intent.putExtra("data_nickname", binding.editNickname.text.toString())
+                }
+                (id.length<6) || (id.length>10)->{
+                    Snackbar.make(binding.root,"아이디는 6~10글자 입니다.",Snackbar.LENGTH_SHORT).show()
+                }
+                (pw.length<8) || (pw.length>12)->{
+                    Snackbar.make(binding.root,"비밀번호는 8~12글자 입니다.",Snackbar.LENGTH_SHORT).show()
+                }
+                else->{
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("data_id", id)
+                    intent.putExtra("data_pw", pw)
+                    intent.putExtra("data_nickname", nickname)
+                    intent.putExtra("data_tmi", tmi)
+                    setResult(RESULT_OK, intent)
+                    finish() //startActivity(intent) // 얘랑 finish()얘랑 차이점이 머임?
+                }
             }
         }
     }
